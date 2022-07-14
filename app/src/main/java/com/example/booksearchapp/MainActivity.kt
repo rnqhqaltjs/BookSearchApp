bbe8ef7c8e6b48843ef33ce10d40c5d32c874260
rnqhqaltjs <rnqhqaltjs@naver.com>
2022-07-15 01:50:14 +0900

    Initial commit

---
+++ app/src/main/java/com/example/booksearchapp/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.example.booksearchapp
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.booksearchapp.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private val binding: ActivityMainBinding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+
+    }
+}
