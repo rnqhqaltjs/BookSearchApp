31e4e610e42870680e08f93331d1635023517fda
rnqhqaltjs <rnqhqaltjs@naver.com>
2022-07-15 02:03:07 +0900

    Initial commit

--- app/src/main/java/com/example/booksearchapp/MainActivity.kt
+++ app/src/main/java/com/example/booksearchapp/MainActivity.kt
@@ -3,6 +3,9 @@ package com.example.booksearchapp
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.booksearchapp.databinding.ActivityMainBinding
+import com.example.booksearchapp.ui.view.FavoriteFragment
+import com.example.booksearchapp.ui.view.SearchFragment
+import com.example.booksearchapp.ui.view.SettingsFragment
 
 class MainActivity : AppCompatActivity() {
     private val binding: ActivityMainBinding by lazy {
@@ -13,5 +16,35 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
+        setupBottomNavigationView()
+        if (savedInstanceState == null) {
+            binding.bottomNavigationView.selectedItemId = R.id.fragment_search
+        }
+    }
+
+    private fun setupBottomNavigationView() {
+        binding.bottomNavigationView.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.fragment_search -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.frame_layout, SearchFragment())
+                        .commit()
+                    true
+                }
+                R.id.fragment_favorite -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.frame_layout, FavoriteFragment())
+                        .commit()
+                    true
+                }
+                R.id.fragment_settings -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.frame_layout, SettingsFragment())
+                        .commit()
+                    true
+                }
+                else -> false
+            }
+        }
     }
 }
